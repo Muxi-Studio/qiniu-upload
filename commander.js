5ef856b5ce26c6819f5d8daea3a8391b747e0c78
fengzi2016 <1820166258@qq.com>
2018-05-31 16:55:16 +0800

    feat: meet unknow bug

--- commander.js
+++ commander.js
@@ -1,39 +1,43 @@
 #!/usr/bin/env node
 'use strict';
+var push = require('./upload');
 var program = require('commander');
-// //命令行参数
+
+
 program
-    .usage('<command> [options]')
-    .version(require('./package.json').version)
+    .version(require('./package.json').version,'-v','--version')
     //需要读取文件的根目录,具体到最后一层文件夹
-    .option('-p, --path <path>','your local path')
+    .option('-p, --path <path>','your local path such as ./')
     //上传文件的空间(bucket)对应的机房
-    .option('-z, --zone <zone>','your online zone')
+    .option('-z, --zone <zone>','your online zone \n'
+       +'there are some options: \n'
+       +'                           华东：qiniu.zone.Zone_z0 \n'
+       +'                           华北：qiniu.zone.Zone_z1 \n'
+       +'                           华南：qiniu.zone.Zone_z2 \n'
+       +'                           北美：qiniu.zone.Zone_na0 \n')
     //上传文件的空间名
-    .option('-b, --bucket <bucket>','your online bucket')
+    .option('-b, --bucket <bucket>','your online bucket such as "mybucket"')
     //上传文件的前缀
-    .option('-pre, --prefix <prefix>','your upload prefix')
+    .option('-pre, --prefix <prefix>','your upload prefix such "test\\"')
+    .parse(process.argv)
+//添加环境变量
+program
+    .command('addkey <ak> <sk>' , 'add environment variables: accesskey secretKey')
+    .alias('addkey <ak> <sk>')
+    .action(function(cmd,ak,sk){
+       process.env.accessKey = ak;
+       process.env.secretKey = sk;
+    })
+    program.parse(process.argv)
 
+program
+    .command('pushq','start to push to qiniu')
+    .alias('pq')
+    .action(function(){
+        push();
+    })
+    program.parse(process.argv)
 
-
-// program
-//     .command('qiniu-upload')
-//     .action(function(){
-//         console.log(program.parse(process.argv).path);
-//     })
-//     program.parse(process.argv);
-
-// console.log()
-console.log(process.argv )
-//process.argv
-//node.exe绝对路径+node所执行文件的绝对路径+所传参数
-// console.log(program.parse(process.argv))
-
-
-
-
-
-// program.parse(process.argv)
-// if(!program.args.length) {
-//     program.help();
-// }
+if (!process.argv.slice(2).length) {
+    program.outputHelp()
+  }
--- upload.js
+++ upload.js
@@ -8,94 +8,92 @@ function warn (arg) {
     console.log(`Warn: please give me the ${arg} !!!`) ;
 }
 
+module.exports = function() {
 //需要用户输入的命令行参数
-var path , zone ,bucket , prefix;
+    var path , zone ,bucket , prefix;
 
-path = argv.path ? argv.path : warn("path");
-zone = argv.zone ? argv.zone : warn("zone");
-bucket = argv.bucket ? argv.bucket : warn("bucket");
-prefix = argv.prefix ? argv.prefix : warn("prefx")
+    path = argv.path ? argv.path : warn("path");
+    zone = argv.zone ? argv.zone : warn("zone");
+    bucket = argv.bucket ? argv.bucket : warn("bucket");
+    prefix = argv.prefix ? argv.prefix : warn("prefx")
 
-// process.env.path = './';
+    // process.env.path = './';
 
-// process.env.zone = 'qiniu.zone.Zone_z0';
+    // process.env.zone = 'qiniu.zone.Zone_z0';
 
-// process.env.bucket = 'test-sdk';
+    // process.env.bucket = 'test-sdk';
 
-// process.env.prefix = 'test/';
-
-
-
-var config = new qiniu.conf.Config();
-// 空间对应的机房
-config.zone = qiniu.zone.Zone_z0;
-
-// 华东：qiniu.zone.Zone_z0
-// 华北：qiniu.zone.Zone_z1
-// 华南：qiniu.zone.Zone_z2
-// 北美：qiniu.zone.Zone_na0
-
-var accessKey = '0bNiwJGpdwmvvuVAzLDjM6gnxj9MiwmSagVpIW81';
-var secretKey = 'zHA9w8PoSfL6D4dvWNwU2GF4XHUn9MalynbANE3_';
+    // process.env.prefix = 'test/';
 
 
-//编写上传回复设置
-var options = {
-    //上次的空间
-    scope: bucket, 
-    //返回的数据形式
-    returnBody: '{"key":"$(key)","hash":"$(etag)","fsize":$(fsize),"bucket":"$(bucket)","name":"$(x:name)"}' 
-}
-// 覆盖默认的上传回复设置
-var putPolicy = new qiniu.rs.PutPolicy(options);
-//根据Key生成独一无二的mac值
-var mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
-//根据mac值生成独一无二的上传token
-var uploadToken=putPolicy.uploadToken(mac);
 
-var formUploader = new qiniu.form_up.FormUploader(config);
-var putExtra = new qiniu.form_up.PutExtra();
+    var config = new qiniu.conf.Config();
+    // 空间对应的机房
+    config.zone = qiniu.zone.Zone_z0;
 
-// 文件上传
-if(path && zone && bucket && prefix)
-getFiles.getFilesUrl(path)
-    .then((files,rejectValue)=>{
-        if(rejectValue){
-            console.log('there is some err on getFilesUrl Funciton and the rejectValue is :' + rejectValue);
-             throw Error();
-        } 
-        //筛选出文件，防止上传文件夹而导致的错误
-      let filesPath = files.filter(file => {
-            let stat = fs.lstatSync(file);
-            if(stat.isFile()) {
-                return file;
-            }else{
-                console.log("Warn!!!it isn't a file that can't to be uploaded :  " + file)
-            }
-        });
-        filesPath.forEach(file => {
-            var key = prefix + file;
-            var localFile =  file;
-            formUploader.putFile(uploadToken, key, localFile, putExtra, function(respErr,
-                respBody, respInfo) {
-                if (respErr) {
-                    throw respErr;
+
+    // var accessKey = '0bNiwJGpdwmvvuVAzLDjM6gnxj9MiwmSagVpIW81';
+    // var secretKey = 'zHA9w8PoSfL6D4dvWNwU2GF4XHUn9MalynbANE3_';
+    accessKey = process.env.accessKey;
+    secretKey = process.env.secretKey;
+
+    //编写上传回复设置
+    var options = {
+        //上次的空间
+        scope: bucket, 
+        //返回的数据形式
+        returnBody: '{"key":"$(key)","hash":"$(etag)","fsize":$(fsize),"bucket":"$(bucket)","name":"$(x:name)"}' 
+    }
+    // 覆盖默认的上传回复设置
+    var putPolicy = new qiniu.rs.PutPolicy(options);
+    //根据Key生成独一无二的mac值
+    var mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
+    //根据mac值生成独一无二的上传token
+    var uploadToken=putPolicy.uploadToken(mac);
+
+    var formUploader = new qiniu.form_up.FormUploader(config);
+    var putExtra = new qiniu.form_up.PutExtra();
+
+    // 文件上传
+    if(path && zone && bucket && prefix)
+    getFiles.getFilesUrl(path)
+        .then((files,rejectValue)=>{
+            if(rejectValue){
+                console.log('there is some err on getFilesUrl Funciton and the rejectValue is :' + rejectValue);
+                throw Error();
+            } 
+            //筛选出文件，防止上传文件夹而导致的错误
+        let filesPath = files.filter(file => {
+                let stat = fs.lstatSync(file);
+                if(stat.isFile()) {
+                    return file;
+                }else{
+                    console.log("Warn!!!it isn't a file that can't to be uploaded :  " + file)
                 }
-            if (respInfo.statusCode == 200) {
-                console.log(respBody);
-            } else {
-                console.log("there may be some err with code :" + respInfo.statusCode +'and Info : ' + respBody)           
-            }
-       });
-   });
-
-    })
-    .catch(err => {
-        console.log('meet err');
+            });
+            filesPath.forEach(file => {
+                var key = prefix + file;
+                var localFile =  file;
+                formUploader.putFile(uploadToken, key, localFile, putExtra, function(respErr,
+                    respBody, respInfo) {
+                    if (respErr) {
+                        throw respErr;
+                    }
+                if (respInfo.statusCode == 200) {
+                    console.log(respBody);
+                } else {
+                    console.log("there may be some err with code :" + respInfo.statusCode +'and Info : ' + respBody)           
+                }
+        });
     });
+
+        })
+        .catch(err => {
+            console.log('meet err');
+        });
    
 
-
+}
 
 
 
